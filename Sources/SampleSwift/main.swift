8a269d6c91ee3aa008bde20a13ae2869fd711c09
Bojan <dbojan@gmail.com>
2020-10-29 22:39:06 -0300

    fix main

--- Sources/SampleSwift/main.swift
+++ Sources/SampleSwift/main.swift
@@ -1,9 +1,10 @@
-import App
 import Vapor
-
-var env = try Environment.detect()
-try LoggingSystem.bootstrap(from: &env)
-let app = Application(env)
+ 
+let app = try Application(.detect())
 defer { app.shutdown() }
-try configure(app)
+
+app.get("hello") { req in
+    return "Hello, world."
+}
+
 try app.run()
