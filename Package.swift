0373e574de74db60551f6b34274faa6a1946bef4
Bojan <dbojan@gmail.com>
2020-10-29 22:29:30 -0300

    vapor

--- Package.swift
+++ Package.swift
@@ -5,12 +5,12 @@ import PackageDescription
 let package = Package(
     name: "SampleSwift",
     dependencies: [
-        .package(name: "Swifter", url: "https://github.com/httpswift/swifter.git", .upToNextMajor(from: "1.5.0"))
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0")
     ],
     targets: [
         .target(
             name: "SampleSwift",
-            dependencies: ["Swifter"],
+            dependencies: ["vapor"],
             path: "./Sources")
     ]
 )
--- Sources/SampleSwift/main.swift
+++ Sources/SampleSwift/main.swift
@@ -1,26 +1,9 @@
-import Swifter
-import Dispatch
+import App
+import Vapor
 
-let server = HttpServer()
-server["/assets/:path"] = shareFilesFromDirectory("assets")
-server["/"] = scopes {
-  html {
-    body {
-      center {
-        img { src = "/assets/sammy.png" }
-        h1 { inner = "Hello from Sammy the Shark!" }
-      }
-    }
-  }
-}
-server["/files/:path"] = directoryBrowser("/")
-
-let semaphore = DispatchSemaphore(value: 0)
-do {
-  try server.start(8080, forceIPv4: true)
-  print("The magic is happening on port: \(try server.port()). Visit http://localhost:8080")
-  semaphore.wait()
-} catch {
-  print("Error starting server: \(error)")
-  semaphore.signal()
-}
+var env = try Environment.detect()
+try LoggingSystem.bootstrap(from: &env)
+let app = Application(env)
+defer { app.shutdown() }
+try configure(app)
+try app.run()
