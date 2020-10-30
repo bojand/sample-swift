d7810b5690180e271f0a5ba795b76007c7bd737c
Bojan <dbojan@gmail.com>
2020-10-29 23:07:00 -0300

    change default configs

--- Sources/SampleSwift/main.swift
+++ Sources/SampleSwift/main.swift
@@ -1,6 +1,10 @@
 import Vapor
  
 let app = try Application(.detect())
+
+app.http.server.configuration.hostname = "0.0.0.0"
+app.http.server.configuration.port = 8080
+
 defer { app.shutdown() }
 
 app.get("hello") { req in
